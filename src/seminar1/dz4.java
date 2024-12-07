f1f5b48650311f5328cd00427009a1fadc201f06
Serg Litov <Serg@litov.com>
2024-12-07 10:42:10 +0300

    dz4 start

---
+++ src/seminar1/dz4.java
@@ -0,0 +1,27 @@
+package seminar1;
+
+class Answer_4 {
+    // Функция для нахождения максимума из двух чисел
+    public int findMaxOfTwo(int a, int b) {
+        return 0;
+    }
+    // Функция для нахождения максимума из трех чисел
+    public int findMaxOfThree(int a, int b, int c) {
+        return 0;
+    }
+}
+// Не удаляйте этот класс - он нужен для вывода результатов на экран и проверки
+public class dz4 {
+    public static void main(String[] args) {
+        int a = 5, b = 10, c = 3;
+        if (args.length == 3) {
+            a = Integer.parseInt(args[0]);
+            b = Integer.parseInt(args[1]);
+            c = Integer.parseInt(args[2]);
+        }
+// Вывод результата на экран
+        Answer_4 ans = new Answer_4();
+        int itresume_res = ans.findMaxOfThree(a, b, c);
+        System.out.println(itresume_res);
+    }
+}
